7652eb46389c3472d10b00986ff99599bdef814a
Aregue RH <arah@hotmail.fr>
2025-01-29 01:16:47 +0100

    ajout de formInscription.php

--- formInscription.php
+++ formInscription.php
@@ -1,1 +1,33 @@
 <?php
+if 
+    ((!isset($_POST["identifiant"])) || (empty($_POST["identifiant"])) || (!isset($_POST["motDePasse"])) || (empty($_POST["motDePasse"])))
+        {
+            $msgErreur = 'veuillez proposer un identifiant et un mot de passe valide';
+            var_dump($msgErreur);
+            header('Location: http://localhost:8000/formInscription.html');
+            exit();
+        }
+else
+        {
+            $identifiant = htmlspecialchars($_POST["identifiant"]);
+            $mdp = $_POST["motDePasse"];
+            $hashed_mdp = password_hash($mdp, PASSWORD_DEFAULT);
+        }
+
+try 
+        {
+            $db = new PDO ('mysql:host=localhost;dbname=bdd_post-it;charset=utf8', 'root', '');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : '.$e->getMessage());
+            
+        }
+        
+$request = $db->prepare("INSERT INTO utilisateur (identifiant, mot_de_passe_hashed) VALUES (?,?) ");
+$request->execute([$identifiant, $hashed_mdp]);
+
+header('Location: http://localhost:8000/test_inscription.html');
+exit();
+?>
